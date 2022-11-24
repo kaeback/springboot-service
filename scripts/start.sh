1cac41f732047169ce54a0e868842a8b1676e2ee
kaeback <kaeback@gmail.com>
2022-11-24 17:01:24 +0900

    11/24 커밋

--- scripts/start.sh
+++ scripts/start.sh
@@ -33,4 +33,4 @@ classpath:/application-$IDLE_PROFILE.yml,\
 /home/ec2-user/app/application-oauth.yml,\
 /home/ec2-user/app/application-real-db.yml \
 -DSpring.profiles.active=$IDLE_PROFILE \
-$JAR_NAME > $REPOSITORY/nohup.out 2>$1 &
+$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
