1a9dc20a0c4e918e9e28cb0250e53b62a54158ba
kaeback <kaeback@gmail.com>
2022-11-24 16:55:56 +0900

    11/24 커밋

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,7 +2,7 @@
 
 ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
-source=${ABSDIR}/profile.sh
+source ${ABSDIR}/profile.sh
 
 REPOSITORY=/home/ec2-user/app/step3
 
@@ -13,10 +13,10 @@ echo "> cp $REPOSITORY/zip/build/libs/*.jar*.jar $REPOSITORY"
 
 cp $REPOSITORY/zip/build/libs/*.jar $REPOSITORY
 
-echo "> 새 애플리케이션 배포"
+echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo "> JAR_NAME: $JAR_NAME"
+echo "> JAR Name: $JAR_NAME"
 
 echo "> $JAR_NAME 에 실행권한 추가"
 
